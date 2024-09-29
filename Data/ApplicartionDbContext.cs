6afbf9400119cbccf714c0fbd5f42e73b88e67e1
Deema-Thnabi <deemathnabi25@gmail.com>
2024-09-29 16:15:02 +0300

    first commit

---
+++ Data/ApplicartionDbContext.cs
@@ -0,0 +1,35 @@
+﻿using CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Data
+{
+    internal class ApplicartionDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = CRUD; Trusted_Connection = True; TrustServerCertificate = True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(P => {
+                P.Property(P => P.Name).HasColumnType("varchar(20)");
+                P.Property(P => P.Price).HasColumnType("decimal(12,2)");
+            });
+
+            modelBuilder.Entity<Order>(P => {
+                P.Property(P => P.Address).HasColumnType("varchar(50)");
+            });
+
+        }
+
+        public DbSet <Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using CRUD.Data;
+using CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicartionDbContext dbContext = new ApplicartionDbContext();
+
+            // add data to product table
+            dbContext.Products.AddRange(new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Laptop",
+                    Price = 999.99m
+                },
+                new Product()
+                {
+                    Name = "Smartphone",
+                    Price = 799.99m
+                },
+                new Product()
+                {
+                    Name = "Tablet",
+                    Price = 499.99m
+                },
+                new Product()
+                {
+                    Name = "Headphones",
+                    Price = 199.99m
+                },
+                new Product()
+                {
+                    Name = "Smartwatch",
+                    Price = 249.99m
+                }
+            });
+            //----------------------------------------------------
+
+            // add data to order table 
+            dbContext.Orders.AddRange(new List<Order>()
+            {
+                new Order()
+                {
+                    Address = "123 Main St",
+                    CreatedAt = DateTime.Now,
+                },
+                new Order()
+                {
+                    Address = "456 Elm St",
+                    CreatedAt = DateTime.Now
+                },
+                new Order()
+                {
+                    Address = "789 Oak St",
+                    CreatedAt = DateTime.Now
+                }
+            });
+            dbContext.SaveChanges();
+
+
+
+            // get all Products 
+            var products = dbContext.Products.AsNoTracking().ToList();
+            products.ForEach(p => Console.WriteLine($"ProductName: {p.Name} and Price is {p.Price}"));
+
+            // get all Orders
+            Console.WriteLine("============================================================");
+            var orders = dbContext.Orders.AsNoTracking().ToList();
+            orders.ForEach(o => Console.WriteLine($"OrderAddress: {o.Address} and Created At {o.CreatedAt}"));
+
+
+            // update product name
+            Console.WriteLine("============================================================");
+            var product = dbContext.Products.FirstOrDefault(p => p.Id == 4);
+
+            if (product is not null)
+            {
+                product.Name = "Lamba";
+            } else Console.WriteLine("No Product");
+
+            dbContext.SaveChanges();
+
+
+
+            // update order created at
+            Console.WriteLine("============================================================");
+            var createdAt = dbContext.Orders.FirstOrDefault(o => o.Id == 2);
+
+            if (createdAt is not null)
+            {
+                createdAt.CreatedAt = DateTime.Now;
+            }
+            else Console.WriteLine("No Order");
+
+            dbContext.SaveChanges();
+
+            // remove product with id 2
+            Console.WriteLine("===============================================");
+            var removeProduct = dbContext.Products.FirstOrDefault(p =>p.Id == 2);
+            if (removeProduct is not null)
+            {
+                dbContext.Products.Remove(removeProduct);
+            }
+            else Console.WriteLine("No Product");
+
+            dbContext.SaveChanges();
+
+            // remove order with id 3
+            Console.WriteLine("===============================================");
+            var removeOrder = dbContext.Orders.FirstOrDefault(o => o.Id == 3);
+            if (removeOrder is not null)
+            {
+                dbContext.Orders.Remove(removeOrder);
+            }
+            else Console.WriteLine("No Order");
+
+            dbContext.SaveChanges(); 
+        }
+    }
+}
